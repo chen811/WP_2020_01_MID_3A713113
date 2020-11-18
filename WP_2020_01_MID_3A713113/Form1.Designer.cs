cd57e643758d4544b74c0e4e1b58696dcfa4e4b0
user <user@DESKTOP-7L4BJMM>
2020-11-19 00:47:29 +0800

    加入專案檔案。

---
+++ WP_2020_01_MID_3A713113/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace WP_2020_01_MID_3A713113
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tex = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(681, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 72);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "送出";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tex
+            // 
+            this.tex.Location = new System.Drawing.Point(544, 253);
+            this.tex.Name = "tex";
+            this.tex.Size = new System.Drawing.Size(58, 25);
+            this.tex.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.PeachPuff;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("標楷體", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(475, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(334, 50);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "由血型看個性~";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pic
+            // 
+            this.pic.Image = global::WP_2020_01_MID_3A713113.Properties.Resources._4;
+            this.pic.Location = new System.Drawing.Point(406, 333);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(491, 357);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pic.TabIndex = 3;
+            this.pic.TabStop = false;
+            this.pic.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(391, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "請輸入您的血型:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(608, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 19);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "型";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1284, 786);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tex);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tex;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WP_2020_01_MID_3A713113/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WP_2020_01_MID_3A713113.Properties;
+
+namespace WP_2020_01_MID_3A713113
+{
+    public partial class Form1 : Form
+    {
+        List<Image> gp = new List<Image>();
+
+        public Form1()
+
+       
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            switch (tex.Text)
+            {
+                case "a":
+                    pic.Image = gp[0];
+                    break;
+                case "b":
+                    pic.Image = gp[1];
+                    break;
+                case "o":
+                    pic.Image = gp[2];
+                    break;
+                case "ab":
+                    pic.Image = gp[3];
+                    break;
+                case "A":
+                    pic.Image = gp[0];
+                    break;
+                case "B":
+                    pic.Image = gp[1];
+                    break;
+                case "O":
+                    pic.Image = gp[2];
+                    break;
+                case "AB":
+                    pic.Image = gp[3];
+                    break;
+            }
+         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gp.Add(Resources._1);
+            gp.Add(Resources._2);
+            gp.Add(Resources._3);
+            gp.Add(Resources._4);
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
